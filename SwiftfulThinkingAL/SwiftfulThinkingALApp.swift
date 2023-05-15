172be1aa7804e2d23a9595a56d21c875434b1588
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-05-15 13:41:14 -0600

    Initial Commit

---
+++ SwiftfulThinkingAL/SwiftfulThinkingALApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulThinkingALApp.swift
+//  SwiftfulThinkingAL
+//
+//  Created by Mitch Andrade on 5/15/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulThinkingALApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
