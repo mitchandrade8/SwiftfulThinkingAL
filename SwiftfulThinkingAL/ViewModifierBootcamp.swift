105825ee101d30d56cf762f4ee6fbb34949f17e3
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-07-11 13:51:33 -0600

    -- Started - ViewsModifiers

--- SwiftfulThinkingAL/ViewModifierBootcamp.swift
+++ SwiftfulThinkingAL/ViewModifierBootcamp.swift
@@ -7,9 +7,47 @@
 
 import SwiftUI
 
+struct DefaultButtonViewModifier: ViewModifier {
+    
+    let backgroundColor: Color
+    
+    func body(content: Content) -> some View {
+        content
+            .font(.headline)
+            .foregroundColor(.white)
+            .frame(height: 55)
+            .frame(maxWidth: .infinity)
+            .background(backgroundColor)
+            .cornerRadius(10)
+            .shadow(radius: 10)
+            .padding()
+    }
+    
+}
+
+extension View {
+    
+    func withDefaultButtonFormatting(backgroundColor: Color = .blue) -> some View {
+        modifier(DefaultButtonViewModifier(backgroundColor: backgroundColor))
+    }
+}
+
 struct ViewModifierBootcamp: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Hello, world!")
+                .withDefaultButtonFormatting()
+            
+            Text("Winning!")
+                .withDefaultButtonFormatting(backgroundColor: .teal)
+            
+            Text("Mastery!")
+                .withDefaultButtonFormatting()
+
+            Text("Growth!")
+                .withDefaultButtonFormatting(backgroundColor: .teal)
+
+        }
     }
 }
 
