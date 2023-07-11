be516016dfa6d22d9d64417a888805a6b74a00cd
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-07-11 13:51:09 -0600

    ViewModifiers - Going over and reviewing how powerful creating viewmodifiers and a extension can make reusability for code

---
+++ SwiftfulThinkingAL/ViewModifierBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  ViewModifierBootcamp.swift
+//  SwiftfulThinkingAL
+//
+//  Created by Mitch Andrade on 7/11/23.
+//
+
+import SwiftUI
+
+struct ViewModifierBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ViewModifierBootcamp()
+}
