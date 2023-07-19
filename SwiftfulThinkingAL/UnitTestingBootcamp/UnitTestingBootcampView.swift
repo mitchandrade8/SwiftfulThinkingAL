27e25f911e34008b46659895ecb6e55c4fe321af
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-07-19 13:57:32 -0600

    -- Start of Unit Testing practice and overview

---
+++ SwiftfulThinkingAL/UnitTestingBootcamp/UnitTestingBootcampView.swift
@@ -0,0 +1,18 @@
+//
+//  UnitTestingBootcampView.swift
+//  SwiftfulThinkingAL
+//
+//  Created by Mitch Andrade on 7/19/23.
+//
+
+import SwiftUI
+
+struct UnitTestingBootcampView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UnitTestingBootcampView()
+}
