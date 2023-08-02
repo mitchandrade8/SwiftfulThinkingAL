38441a6b4b90939d9708a0f2b680533cae7646c8
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-08-01 20:55:51 -0600

    -- Wakatime is not picking up on Xcode updates

--- SwiftfulThinkingAL/ButtonStyleBootcamp.swift
+++ SwiftfulThinkingAL/ButtonStyleBootcamp.swift
@@ -9,13 +9,30 @@ import SwiftUI
 
 struct ButtonPressableStyle: ButtonStyle {
     
+    let scaledAmount: CGFloat
+    
+    init(scaledAmount: CGFloat = 0.9) {
+        self.scaledAmount = scaledAmount
+    }
+    
     func makeBody(configuration: Configuration) -> some View {
         configuration.label
+            .scaleEffect(configuration.isPressed ? scaledAmount : 1.0)
+//            .brightness(configuration.isPressed ? 0.80 : 0)
+//            .opacity(configuration.isPressed ? 0.7 : 1.0)
     }
     
 }
 
+extension View {
+    
+    func withPressableStyle() -> some View {
+        self.buttonStyle(ButtonPressableStyle())
+    }
+}
+
 struct ButtonStyleBootcamp: View {
+    
     var body: some View {
         Button(action: {
             
